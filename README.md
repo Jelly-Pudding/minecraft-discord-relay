5715d1d1396f59e7ea2b21c1cd27f0cbd56f740e
Jelly-Pudding <samwalters11111@hotmail.com>
2024-10-13 23:13:05 +0100

    minor improvements

---
+++ README.md
@@ -0,0 +1,58 @@
+# DiscordRelay Plugin
+
+**DiscordRelay** is a Minecraft Paper 1.21.1 plugin that creates a bidirectional chat bridge between your Minecraft server and a Discord channel.
+
+## Features
+- Relay chat messages from Minecraft to Discord and vice versa.
+- Display player join and leave events in Discord.
+- Show player avatars in Discord messages.
+- `/list` command in Discord to see online Minecraft players
+- Server start-up and shutdown notifications in Discord.
+
+## Installation
+1. Download the latest release [here](https://github.com/Jelly-Pudding/minecraft-discord-relay/releases/latest).
+2. Place the `.jar` file in your Minecraft server's `plugins` folder.
+3. Restart your server to generate the default configuration file.
+4. Set up your Discord bot (see [Discord Bot Setup](#discord-bot-setup)).
+5. Configure the plugin (see [Configuration](#configuration)).
+6. Restart your server again or use the [reload command](#in-game-commands).
+
+## Discord Bot Setup
+1. Go to the [Discord Developer Portal](https://discord.com/developers/applications).
+2. Click "New Application" and give it any suitable name. You could call it "MC Bot" for example.
+3. Go to the "Bot" tab and click "Reset Token".
+4. You may have to carry out two-factor authentication. Once you see the new token, copy it to a notepad file.
+5. In the "Bot" tab, scroll down to the "Privileged Gateway Intents" section. Enable "MESSAGE CONTENT INTENT" and "SERVER MEMBERS INTENT".
+6. Go to the "OAuth2" tab, then "OAuth2 URL Generator".
+7. In "Scopes", select "bot". This will make a "Bot Permissions" section appear below.
+8. In "Bot Permissions", select:
+   - Read Message History
+   - View Channels
+   - Send Messages
+9. Copy the generated URL at the bottom of the page. Paste it into the server you want to add your bot to. You should see a message - click it and follow the steps to add your bot to your Discord server.
+10. At the bottom of Discord, you should see your avatar. A bit to the right of this, there is a cog wheel which says, "User Settings" if you hover over it. Click the cog wheel.
+11. In `App Settings`, scroll down to `Advanced`. In the `Advanced` section, enable `Developer Mode`.
+12. In your Discord server, right click the channel where you want messages to be relayed. Copy the channel ID and put it into a notepad file.
+13. Enter the values you obtained for your Discord's `Bot Token` and `Channel ID` into `plugins/DiscordRelay/config.yml`.
+
+## Configuration
+1. Open the `plugins/DiscordRelay/config.yml` file.
+2. Set `discord-bot-token` to your bot's token.
+3. Set `discord-channel-id` to the ID of the Discord channel you want to use for the relay.
+   (To get the channel ID, enable Developer Mode in Discord settings, then right-click the channel and select "Copy ID")
+4. Save the file.
+
+Example `config.yml`:
+```yaml
+discord-bot-token: 'YOUR_BOT_TOKEN_HERE'
+discord-channel-id: 'YOUR_CHANNEL_ID_HERE'
+```
+
+## In-game Commands
+`/discordrelay reload`: Reloads the plugin configuration.
+
+## Discord Commands
+`/list`: Shows the list of online Minecraft players.
+
+## Support Me
+Donations to my [Patreon](https://www.patreon.com/lolwhatyesme) will help with the development of this project.
--- src/main/java/com/jellypudding/discordRelay/DiscordRelay.java
+++ src/main/java/com/jellypudding/discordRelay/DiscordRelay.java
@@ -4,22 +4,34 @@ import io.papermc.paper.event.player.AsyncChatEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.awt.Color;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class DiscordRelay extends JavaPlugin implements Listener {
+public class DiscordRelay extends JavaPlugin implements Listener, TabCompleter {
 
     private JDA jda;
     private String discordChannelId;
@@ -28,9 +40,12 @@ public class DiscordRelay extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         loadConfig();
-
+        connectToDiscord(false);
         getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(getCommand("discordrelay")).setTabCompleter(this);
+    }
 
+    private void connectToDiscord(boolean isReload) {
         try {
             jda = JDABuilder.createDefault(getConfig().getString("discord-bot-token"))
                     .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
@@ -39,8 +54,9 @@ public class DiscordRelay extends JavaPlugin implements Listener {
             jda.awaitReady();
             getLogger().info("Discord bot connected successfully!");
 
-            // Send server start message
-            sendToDiscord("**Server is starting up!**");
+            if (!isReload) {
+                sendToDiscord("**Server is starting up!**");
+            }
         } catch (Exception e) {
             getLogger().severe("Failed to connect to Discord: " + e.getMessage());
             getServer().getPluginManager().disablePlugin(this);
@@ -51,7 +67,7 @@ public class DiscordRelay extends JavaPlugin implements Listener {
     public void onDisable() {
         if (jda != null) {
             try {
-                // Send server stop message synchronously
+                // Send shutdown message synchronously
                 TextChannel channel = jda.getTextChannelById(discordChannelId);
                 if (channel != null) {
                     channel.sendMessage("**Server is shutting down!**").complete();
@@ -59,14 +75,18 @@ public class DiscordRelay extends JavaPlugin implements Listener {
 
                 jda.removeEventListener(jda.getRegisteredListeners());
                 jda.shutdownNow();
-                if (!jda.awaitShutdown(Duration.ofSeconds(5))) {
-                    getLogger().warning("JDA did not shut down in time. Forcing shutdown.");
+                try {
+                    // Wait for shutdown with a shorter timeout
+                    if (!jda.awaitShutdown(Duration.ofSeconds(2))) {
+                        getLogger().warning("JDA did not shut down in time. Forcing shutdown.");
+                        jda.shutdown();
+                    }
+                } catch (InterruptedException e) {
+                    getLogger().warning("Interrupted while shutting down JDA. Forcing shutdown.");
                     jda.shutdown();
+                    Thread.currentThread().interrupt();
                 }
                 getLogger().info("Discord bot disconnected successfully.");
-            } catch (InterruptedException e) {
-                getLogger().warning("Interrupted while shutting down JDA: " + e.getMessage());
-                Thread.currentThread().interrupt();
             } catch (Exception e) {
                 getLogger().warning("Error during JDA shutdown: " + e.getMessage());
             }
@@ -83,28 +103,57 @@ public class DiscordRelay extends JavaPlugin implements Listener {
         if (event.isAsynchronous()) {
             String playerName = event.getPlayer().getName();
             String message = PlainTextComponentSerializer.plainText().serialize(event.message());
-            String formattedMessage = String.format("**%s**: %s", playerName, message);
-            sendToDiscord(formattedMessage);
+            sendPlayerMessageToDiscord(playerName, message);
         } else {
             getServer().getScheduler().runTaskAsynchronously(this, () -> {
                 String playerName = event.getPlayer().getName();
                 String message = PlainTextComponentSerializer.plainText().serialize(event.message());
-                String formattedMessage = String.format("**%s**: %s", playerName, message);
-                sendToDiscord(formattedMessage);
+                sendPlayerMessageToDiscord(playerName, message);
             });
+        }
+    }
+
+    private void sendPlayerMessageToDiscord(String playerName, String message) {
+        if (jda != null) {
+            TextChannel channel = jda.getTextChannelById(discordChannelId);
+            if (channel != null) {
+                String avatarUrl = String.format("https://mc-heads.net/avatar/%s", playerName);
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor(playerName, null, avatarUrl)
+                        .setDescription(message)
+                        .setColor(Color.YELLOW);
+                channel.sendMessageEmbeds(embed.build()).queue();
+            } else {
+                getLogger().warning("Discord channel not found!");
+            }
+        }
+    }
+
+    private void sendPlayerEventToDiscord(String playerName, String action, Color color) {
+        if (jda != null) {
+            TextChannel channel = jda.getTextChannelById(discordChannelId);
+            if (channel != null) {
+                String avatarUrl = String.format("https://mc-heads.net/avatar/%s", playerName);
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor(playerName + " " + action, null, avatarUrl)
+                        .setColor(color);
+                channel.sendMessageEmbeds(embed.build()).queue();
+            } else {
+                getLogger().warning("Discord channel not found!");
+            }
         }
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        String joinMessage = String.format("**%s joined the game**", event.getPlayer().getName());
-        sendToDiscord(joinMessage);
+        String playerName = event.getPlayer().getName();
+        sendPlayerEventToDiscord(playerName, "joined the game", Color.GREEN);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        String quitMessage = String.format("**%s left the game**", event.getPlayer().getName());
-        sendToDiscord(quitMessage);
+        String playerName = event.getPlayer().getName();
+        sendPlayerEventToDiscord(playerName, "left the game", Color.RED);
     }
 
     private void sendToDiscord(String message) {
@@ -118,17 +167,76 @@ public class DiscordRelay extends JavaPlugin implements Listener {
         }
     }
 
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("discordrelay")) {
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission("discordrelay.reload")) {
+                    reloadPlugin();
+                    sender.sendMessage("DiscordRelay plugin reloaded.");
+                    return true;
+                } else {
+                    sender.sendMessage("You don't have permission to reload the plugin.");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void reloadPlugin() {
+        if (jda != null) {
+            jda.shutdown();
+        }
+        reloadConfig();
+        loadConfig();
+        connectToDiscord(true);
+    }
+
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase("discordrelay")) {
+            if (args.length == 1) {
+                List<String> completions = new ArrayList<>();
+                completions.add("reload");
+                return completions;
+            }
+        }
+        return null;
+    }
+
     private class DiscordListener extends ListenerAdapter {
         @Override
         public void onMessageReceived(MessageReceivedEvent event) {
             if (event.getChannel().getId().equals(discordChannelId) && !event.getAuthor().isBot()) {
-                Member member = event.getMember();
-                String name = (member != null && member.getNickname() != null) ? member.getNickname() : event.getAuthor().getName();
-                String message = String.format("§9[Discord] §6%s§f: %s", name, event.getMessage().getContentDisplay());
-                Bukkit.getScheduler().runTask(DiscordRelay.this, () ->
-                        Bukkit.broadcast(net.kyori.adventure.text.Component.text(message))
-                );
+                String content = event.getMessage().getContentRaw();
+                if (content.equalsIgnoreCase("/list")) {
+                    sendPlayerList(event.getChannel());
+                } else {
+                    Member member = event.getMember();
+                    String name = (member != null && member.getNickname() != null) ? member.getNickname() : event.getAuthor().getName();
+                    String message = String.format("§9[Discord] §6%s§f: %s", name, event.getMessage().getContentDisplay());
+                    Bukkit.getScheduler().runTask(DiscordRelay.this, () ->
+                            Bukkit.broadcast(net.kyori.adventure.text.Component.text(message))
+                    );
+                }
             }
+        }
+
+        private void sendPlayerList(MessageChannel channel) {
+            List<String> playerNames = Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .collect(Collectors.toList());
+
+            String playerListString = playerNames.isEmpty() ? "No players online." : String.join(", ", playerNames);
+            String message = String.format("Online players (%d): %s", playerNames.size(), playerListString);
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("Player List")
+                    .setDescription(message)
+                    .setColor(Color.BLUE);
+
+            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 }
